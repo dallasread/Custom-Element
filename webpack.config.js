090720412add28d18fbda1d197b5e4ad4c58e4de
Dallas Read <dallas@excitecreative.ca>
2018-12-19 20:33:36 -0400

    Webpack compatibility.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var js = {
     entry: ['./index.js'],
-    output: { filename: './demo/custom-element.js' },
+    output: { filename: '../demo/custom-element.js' },
     module: {
-        loaders: [
-            { test: /\.json$/, loader: 'json-loader' },
+        rules: [
+            // { test: /\.json$/, loader: 'json-loader' },
             { test: /\.html$/, loader: 'html-loader?minimize=false' },
             { test: /\.png$/,  loader: 'url-loader?mimetype=image/png' },
             { test: /\.jpeg$/, loader: 'url-loader?mimetype=image/jpeg' },
